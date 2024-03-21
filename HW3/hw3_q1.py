971ac53f81c54f89d1ca0635e8786bbff41bbcee
AngelMM26 <130812286+AngelMM26@users.noreply.github.com>
2024-03-21 01:08:25 -0400

    Create hw3_q1.py

---
+++ HW3/hw3_q1.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Loads and reads Dataframe
+url = "https://data.cityofnewyork.us/api/views/6fi9-q3ta/rows.csv?accessType=DOWNLOAD"
+df = pd.read_csv(url)
+
+#Gets the day from the hour_beginning column
+df['hour_beginning'] = pd.to_datetime(df['hour_beginning'])
+df['day'] = df['hour_beginning'].dt.day_name()
+
+#Reorders the index 
+df = df.sort_values(by='hour_beginning')
+df.reset_index(drop=True, inplace=True)
+
+#Fixes missing data
+df['temperature'] = df['temperature'].ffill()
+df['precipitation'] = df['precipitation'].ffill()
+df['weather_summary'] = df['weather_summary'].ffill()
+
+week_day = df[(df['day'] != 'Sunday') & (df['day'] != 'Saturday')]
+
+plt.figure(figsize=(10, 5))
+plt.plot(week_day, df['Pedestrians'], color='blue')
+plt.title('Pedestrian Counts Over Time')
+plt.xlabel('Day')
+plt.ylabel('Pedestrian Count')
+plt.grid(True)
+plt.tight_layout()
+print(plt.show())
