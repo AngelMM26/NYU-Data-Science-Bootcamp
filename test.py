11c8c162eaa6a8edfcbbc313109b915a011a7ab5
AngelMM26 <130812286+AngelMM26@users.noreply.github.com>
2024-04-04 08:40:52 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('AB_NYC_2019.csv')
+borough = {"Manhattan" : {}, "Brooklyn" : {}, "Bronx" : {}, "Queens" : {}, "Staten Island" : {}}
+
+def updatePrice(borough, neighbourhood_group, neighbourhood, price):
+    if neighbourhood not in borough:
+        borough[neighbourhood_group][neighbourhood] = price
+    else:
+        borough[neighbourhood_group][neighbourhood] += price
+
+maxRange = 48895
+for currRow in range(0, maxRange):
+    neighbourhood_group = df.loc[currRow]["neighbourhood_group"]
+    neighbourhood = df.loc[currRow]["neighbourhood"]
+    price = df.loc[currRow]["price"]
+    updatePrice(borough, neighbourhood_group, neighbourhood, price)
+    
+print(borough)
+
